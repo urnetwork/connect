bb1b1802673c0ea3028c5402a378132b12c0cc23
Lil Widget <widget@builder.localdomain>
2025-04-03 02:20:01 -0700

    2025.4.3-58871996

--- v2025/frame.go
+++ v2025/frame.go
@@ -5,7 +5,7 @@ import (
 
 	"google.golang.org/protobuf/proto"
 
-	"github.com/urnetwork/connect/protocol"
+	"github.com/urnetwork/connect/protocol/v2025"
 )
 
 func ToFrame(message proto.Message) (*protocol.Frame, error) {
