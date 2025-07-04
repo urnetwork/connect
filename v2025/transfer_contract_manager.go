1200ba14c0a5ce7a2b19874970f95804ce950260
Product Builder <builder@bringyour.com>
2025-07-04 15:53:51 -0700

    2025.7.4-668695950

--- v2025/transfer_contract_manager.go
+++ v2025/transfer_contract_manager.go
@@ -21,7 +21,7 @@ import (
 
 	"github.com/golang/glog"
 
-	"github.com/urnetwork/connect/protocol"
+	"github.com/urnetwork/connect/v2025/protocol"
 )
 
 // manage contracts which are embedded into each transfer sequence
