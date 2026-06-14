3e0e2d6200fe9b3cfb7fc48c67df60071841909e
Brien Colwell <xcolwell@gmail.com>
2026-06-13 22:50:16 -0500

    transport_p2p: unbreak js build

--- transport_p2p_webrtc_pc_js.go
+++ transport_p2p_webrtc_pc_js.go
@@ -11,8 +11,10 @@ import (
 )
 
 func createWebRtcPeerConnection(ctx context.Context, active bool, settings *WebRtcSettings) (*webrtc.PeerConnection, error) {
+	// The js/wasm SettingEngine wraps the browser's native WebRTC and exposes
+	// none of the tuning the non-js build uses (LoggerFactory, SCTP/MTU/ICE
+	// timeouts). Setting any of those here does not compile for GOOS=js.
 	s := webrtc.SettingEngine{}
-	s.LoggerFactory = &pionLoggerFactory{log: loggerOrDefault(settings.Log)}
 
 	api := webrtc.NewAPI(webrtc.WithSettingEngine(s))
 	return api.NewPeerConnection(webrtc.Configuration{
