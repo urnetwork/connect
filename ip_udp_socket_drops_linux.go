8fcb05543159f5f5f973e33c51e5ad1db024feaf
Product Builder <builder@bringyour.com>
2026-09-17 15:06:30 -0700

    ip: use portable Linux getsockopt syscall

--- ip_udp_socket_drops_linux.go
+++ ip_udp_socket_drops_linux.go
@@ -4,8 +4,9 @@ package connect
 
 import (
 	"net"
-	"syscall"
 	"unsafe"
+
+	"golang.org/x/sys/unix"
 )
 
 // SO_MEMINFO returns nine u32 counters for a socket; the last is sk_drops,
@@ -23,13 +24,15 @@ func udpSocketReceiveDropCount(socket net.Conn) uint64 {
 		return 0
 	}
 	var counters [9]uint32
-	var errno syscall.Errno
+	var errno unix.Errno
 	controlErr := rawConn.Control(func(fd uintptr) {
 		length := uint32(unsafe.Sizeof(counters))
-		_, _, errno = syscall.Syscall6(
-			syscall.SYS_GETSOCKOPT,
+		// The frozen syscall package does not expose SYS_GETSOCKOPT on every
+		// Linux architecture (notably 386); x/sys/unix does.
+		_, _, errno = unix.Syscall6(
+			unix.SYS_GETSOCKOPT,
 			fd,
-			syscall.SOL_SOCKET,
+			unix.SOL_SOCKET,
 			soMemInfo,
 			uintptr(unsafe.Pointer(&counters[0])),
 			uintptr(unsafe.Pointer(&length)),
